0497496833bb84d4022e38902fc47787bd5cbde4
NileyGF <nileygf@gmail.com>
2024-12-12 13:02:15 -0500

    move main to /src

--- main.py
+++
@@ -1,8 +0,0 @@
-import chatbot.base_chat as base_chat
-
-model = base_chat.Base_Chat()
-print("AI: Hello.")
-while True:
-    user_input = input("User: ")
-    response = model.chat_with_history(user_input)
-    
---
+++ src/main.py
@@ -0,0 +1,11 @@
+# import chatbot.base_chat as base_chat
+
+# model = base_chat.Base_Chat()
+# print("AI: Hello.")
+# while True:
+#     user_input = input("User: ")
+#     response = model.chat_with_history(user_input)
+
+import superhacks.generate_superhacks as sh
+
+sh.determine_best_clustering_and_hyperparameters()
